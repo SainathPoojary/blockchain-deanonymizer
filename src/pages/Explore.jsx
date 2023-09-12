4ec8276f6a5728270806313b045fdcccc2d2f7bf
Sainath Poojary <spoojary614@gmail.com>
2023-09-13 04:01:15 +0530

    Added Explore Page

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,5 +1,48 @@
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
+import Navbar from "../components/Navbar";
+import Profile from "../components/Profile";
+import Transaction from "../components/Transactions";
+import data from "../data.json";
 
 export default function Explore() {
-  return <div>Explore</div>;
+  const [walletAddress, setWalletAddress] = useState(
+    "36mwz7xBbw4YVteng6iZaTDdBGsK5pWGQG"
+  );
+
+  const [walletData, setWalletData] = useState(data);
+  const [loading, setLoading] = useState(true);
+
+  const getWalletData = async () => {
+    setLoading(true);
+    // const response = await fetch(
+    //   `https://blockchain.info/rawaddr/${walletAddress}`
+    // );
+    // const data = await response.json();
+    // console.log(data);
+    // setWalletData(data);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getWalletData();
+    console.log(walletAddress);
+  }, [walletAddress]);
+
+  // const memoizedWalletData = useMemo(() => walletData, [walletAddress]);
+
+  return (
+    <div>
+      <Navbar setWalletAddress={setWalletAddress} />
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          <Profile walletData={walletData} />
+          <Transaction
+            walletData={walletData}
+            setWalletAddress={setWalletAddress}
+          />
+        </div>
+      )}
+    </div>
+  );
 }
